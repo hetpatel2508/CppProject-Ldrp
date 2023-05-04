4fa31693e18c1402a346f2b327105f3d95122dd1
hetpatel2508 <125752460+hetpatel2508@users.noreply.github.com>
2023-05-04 11:21:01 +0530

    FirstInitialize

---
+++ MainLobbyDesign.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    cout<<endl;
+   cout<<"****************************************************************"<<endl;
+   cout<<"*    Welcome To LDRP Institute of Technology and Research      *"<<endl;
+   cout<<"****************************************************************"<<endl;
+   cout<<"*                                                              *"<<endl;
+   cout<<"*    Press 1: Add Student Details                              *"<<endl;
+   cout<<"*    Press 2: Get All Student's Details                        *"<<endl;
+   cout<<"*    Press 3: Get Particular Student's Details                 *"<<endl;
+   cout<<"*    Press 4: Replace Student Details                          *"<<endl;
+   cout<<"*    Press 5: Delete Student Details                           *"<<endl;
+   cout<<"*                                                              *"<<endl;
+   cout<<"****************************************************************"<<endl;
+   cout<<"                                        Operated By:Patel Het R."<<endl;
+   cout<<"                                             E.Num.:22BECE30273 "<<endl<<endl<<endl<<endl<<endl;
+}
